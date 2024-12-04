fd2db2c7bedb59c0f79d40f1201983dedfc711ec
phucnhan <phucnhan0406@gmail.com>
2024-12-05 03:42:25 +0700

    update fitness page
    update giao diện, truy xuất, filter, header

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import json
 
 app = Flask(__name__)
@@ -13,17 +13,32 @@ data = read_data('minified-exercises.json')
 def index():
     categories = set()
     primary_muscles = set()
-    
     for exercise in data['exercises']:
         categories.add(exercise['category'])
         primary_muscles.update(exercise.get('primary_muscles', []))
-    
-    return render_template('index.html', exercises=data['exercises'], categories=categories, primary_muscles=primary_muscles)
 
-@app.route('/exercise/<int:exercise_id>')
-def exercise(exercise_id):
-    exercise = data['exercises'][exercise_id]
-    return render_template('exercise.html', exercise=exercise)
+    first_exercises = data['exercises'][:10]
+    return render_template(
+        'index.html',
+        categories=categories,
+        primary_muscles=primary_muscles,
+        first_exercises=first_exercises
+    )
+
+@app.route('/filter', methods=['GET'])
+def filter_exercises():
+    category = request.args.get('category', '')
+    muscle = request.args.get('muscle', '')
+    equipment = request.args.get('equipment', '')
+
+    filtered_exercises = [
+        exercise for exercise in data['exercises']
+        if (category == '' or exercise['category'] == category) and
+           (muscle == '' or any(m in exercise.get('primary_muscles', []) for m in muscle.split(','))) and
+           (equipment == '' or equipment in exercise.get('equipment', []))
+    ]
+
+    return jsonify(filtered_exercises)
 
 if __name__ == '__main__':
     app.run(debug=True)
